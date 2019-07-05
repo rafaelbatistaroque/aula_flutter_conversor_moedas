2634da99c3ff0147b8c80e870e0d8aadf8e4efce
rafaelbatistaroque <maranata_rafa@hotmail.com>
2019-07-05 16:07:23 -0400

    Added inputBox currency Real

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,24 +7,30 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  double dolar, euro;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
+      
+      //Barra superior
       appBar: AppBar(
         title: Text("Conversor de moedas"),
         backgroundColor: Colors.amber,
         centerTitle: true),
-
-      body: FutureBuilder<Map>(
+      
+      //Corpo do App
+      body: FutureBuilder<Map>(  
         future: getData(),
         builder: (context, snapshot) {
           switch(snapshot.connectionState){
             case ConnectionState.none:
             case ConnectionState.waiting:
               return Center(
-                  child: Text("Carregando dados...",
-                    style: TextStyle(color: Colors.amber,fontSize: 20),
+                child: Text("Carregando dados...",
+                    style: TextStyle(color: Colors.amber, fontSize: 20),
                     textAlign: TextAlign.center)
               );
             default:
@@ -32,13 +38,31 @@ class _HomeState extends State<Home> {
                 return Center(
                   child: Text("Erro ao carregar...",
                     style: TextStyle(color: Colors.amber,fontSize: 20),
-                  textAlign: TextAlign.center),
+                    textAlign: TextAlign.center),
                 );
               }else{
-                return Container(color: Colors.green);
+                // dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                // euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+                
+                return SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Icon(Icons.monetization_on,size: 120.0, color: Colors.amber),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: "Real",
+                          labelStyle: TextStyle(fontSize: 16, color: Colors.amber),
+                          border: OutlineInputBorder(),
+                          prefixText: "R\$"
+                        ),
+                      )
+                    ],
+                  ),
+                );
               }
           }
-        })
+        }),
     );
   }
 }
